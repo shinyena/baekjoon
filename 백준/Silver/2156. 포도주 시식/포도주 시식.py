788da9c7fd4ae8ffc3e93e6272b2b7e269affcc7
Daisy <tisdpsk98@gmail.com>
2022-09-19 19:33:22 +0900

    [Silver I] Title: 포도주 시식, Time: 480 ms, Memory: 32152 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,21 @@
+n = int(input())
+grapes = {}
+for i in range(1, n+1):
+    grapes[i] = int(input())
+
+dp = {}
+
+if n >= 1:
+    dp[1] = grapes[1]
+if n >= 2:
+    dp[2] = grapes[1] + grapes[2]
+if n >= 3:
+    dp[3] = max(grapes[1] + grapes[2], grapes[2] + grapes[3], grapes[1] + grapes[3])
+
+
+
+for x in range(4, n+1):
+    dp[x] = max(grapes[x] + grapes[x-1] + dp[x-3], grapes[x] + dp[x-2])
+    dp[x] = max(dp[x], dp[x-1])
+
+print(dp[n])
